01f14e63c8c98be2696ec9b60e65b24a0a808e5f
angx7 <angelmirocu@gmail.com>
2025-06-05 20:30:55 -0600

    creating artist table and adding year column

---
+++ Sources/music-api/Migrations/CreateArtist.swift
@@ -0,0 +1,14 @@
+import Fluent
+
+struct CreateArtist : AsyncMigration{
+    func prepare(on database: any FluentKit.Database) async throws {
+        try await database.schema("artists")
+            .id()
+            .field("name", .string, .required)
+            .create()
+    }
+    
+    func revert(on database: any Database) async throws {
+        try await database.schema("artists").delete()
+    }
+}
